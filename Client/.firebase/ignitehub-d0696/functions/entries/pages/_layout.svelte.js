a9514aa06890012513ec27aac6acdb11c15888ed
Paul Minniti <pminniti@gmail.com>
2023-10-04 02:14:24 -0400

    untrack firebase

--- Client/.firebase/ignitehub-d0696/functions/entries/pages/_layout.svelte.js
+++
@@ -1,8 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const app = "";
-const Layout = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `  <div class="min-h-screen flex flex-col">${slots.default ? slots.default({}) : ``}</div>`;
-});
-export {
-  Layout as default
-};
--- Client/.firebase/ignitehub-d0696/functions/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.4584f9fa.js","_app/immutable/chunks/scheduler.92391968.js","_app/immutable/chunks/index.0ef68b7d.js","_app/immutable/chunks/stores.ccac1364.js","_app/immutable/chunks/singletons.7d40e15a.js","_app/immutable/chunks/index.7c43458a.js"];
-export const stylesheets = [];
-export const fonts = [];
