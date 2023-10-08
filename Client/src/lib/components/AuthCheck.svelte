20b67a6792fc0e43faaa0877e5278419afb3d190
Paul Minniti <pminniti@gmail.com>
2023-10-08 13:43:23 -0400

    auth check for dashboard

--- Client/src/lib/components/AuthCheck.svelte
+++ Client/src/lib/components/AuthCheck.svelte
@@ -3,12 +3,24 @@
 	import { goto } from "$app/navigation";
 	import { user } from "$lib/firebase";
 	import { onMount } from "svelte";
-	import Button from "./common/Button.svelte";
+	import Access from "$lib/assets/access_denied.svg";
 </script>
 
 {#if $user}
 	<slot />
 {:else}
-	<p class="text-error my-10">You dont have permission to be here.</p>
-	<Button href="/">Go back</Button>
+	<div class="container mx-auto mt-16 p-2 m-2 w-3/5 space-y-8 text-center">
+		<div class="relative mt-10 -mx-4 md:-mx-12 lg:mt-0 lg:col-start-1">
+			<img
+				src={Access}
+				alt="illustration"
+				class="relative w-auto max-h-60 mx-auto" />
+		</div>
+		<p class="h2">Whoops!</p>
+		<p class="h3 text-error my-10">
+			You must sign in to access your Dashboard.
+		</p>
+		<a href="/" class="m-2 btn btn-xl variant-filled-secondary text-white">
+			Back to Login</a>
+	</div>
 {/if}
--- Client/src/routes/dashboard/+page.svelte
+++ Client/src/routes/dashboard/+page.svelte
@@ -1,8 +1,11 @@
 <!-- @format -->
 <script lang="ts">
 	import type { PageData } from "./$types";
-	import Navbar from "./Navbar.svelte";
 	export let data: PageData;
 </script>
 
-<!-- <Navbar /> -->
+<p>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus cum
+	saepe laudantium, neque asperiores error beatae! Maxime, nihil eius facilis
+	eaque soluta expedita perferendis. Enim, assumenda. Obcaecati quod vero odio.
+</p>
