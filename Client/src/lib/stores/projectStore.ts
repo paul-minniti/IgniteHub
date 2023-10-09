80e0e7008a350bdce15490aae47082a13328dd07
Paul Minniti <pminniti@gmail.com>
2023-10-09 03:49:32 -0400

    add project page

--- Client/src/lib/stores/projectStore.ts
+++ Client/src/lib/stores/projectStore.ts
@@ -1,54 +1,53 @@
-import { onAuthStateChanged, type User } from "firebase/auth";
-import { writable, type Readable, derived } from "svelte/store";
-import { docStore } from "./docStore";
-import { auth } from "$lib/firebase";
+// import { onAuthStateChanged, type User } from "firebase/auth";
+// import { writable, type Readable, derived } from "svelte/store";
+// import { docStore } from "./docStore";
+// import { auth } from "$lib/firebase";
 
-/**
- * @returns a store with the current firebase user
- */
-function userStore() {
-	let unsubscribe: () => void;
+// /**
+//  * @returns a store with the current firebase project
+//  */
+// function projectStore() {
+// 	let unsubscribe: () => void;
 
-	if (!auth || !globalThis.window) {
-		console.warn("Auth is not initialized or not in browser");
-		const { subscribe } = writable<User | null>(null);
-		return {
-			subscribe
-		};
-	}
+// 	if (!auth || !globalThis.window) {
+// 		console.warn("Auth is not initialized or not in browser");
+// 		const { subscribe } = writable<User | null>(null);
+// 		return {
+// 			subscribe
+// 		};
+// 	}
 
-	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
-		unsubscribe = onAuthStateChanged(auth, (user) => {
-			set(user);
-		});
+// 	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+// 		unsubscribe = onAuthStateChanged(auth, (project) => {
+// 			set(project);
+// 		});
 
-		return () => unsubscribe();
-	});
+// 		return () => unsubscribe();
+// 	});
 
-	return {
-		subscribe
-	};
-}
+// 	return {
+// 		subscribe
+// 	};
+// }
 
-const user = userStore();
+// // const project = projectStore();
 
-interface UserData {
-	email: string;
-	display_name: string;
-	photo_url: string;
-	metadata: {
-		lastSignInTime: string;
-		creationTime: string;
-	};
-}
+// interface ProjectData {
+// 	owner: string;
+// 	project_id: string;
+// 	project_name: string;
+// 	users: Array<string>;
+// 	admins: Array<string>;
+// 	settings: object;
+// }
 
-export const userData: Readable<UserData | null> = derived(
-	user,
-	($user, set) => {
-		if ($user) {
-			return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
-		} else {
-			set(null);
-		}
-	}
-);
+// export const projectData: Readable<ProjectData | null> = derived(
+// 	user,
+// 	($project, set) => {
+// 		if ($project) {
+// 			return docStore<ProjectData>(`projects/${$project.uid}`).subscribe(set);
+// 		} else {
+// 			set(null);
+// 		}
+// 	}
+// );
--- Client/src/routes/dashboard/+page.svelte
+++ Client/src/routes/dashboard/+page.svelte
@@ -4,8 +4,23 @@
 	export let data: PageData;
 </script>
 
-<p class="">
-	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus cum
-	saepe laudantium, neque asperiores error beatae! Maxime, nihil eius facilis
-	eaque soluta expedita perferendis. Enim, assumenda. Obcaecati quod vero odio.
-</p>
+<div class="flex flex-wrap mt-4">
+	<div
+		class="relative w-64 p-4 overflow-hidden bg-white hover:variant-soft-primary hover:cursor-pointer shadow-lg rounded-2xl m-2">
+		<div class="justify-center items-center text-center">
+			<p class="mb-2 text-lg font-medium text-primary-500">
+				<i class="fa-solid fa-plus" />
+			</p>
+			<p class="text-xl font-medium text-primary-500">Add Project</p>
+		</div>
+	</div>
+
+	<div
+		class="relative w-64 p-4 overflow-hidden bg-white shadow-lg rounded-2xl m-2">
+		<div class="">
+			<p class="mb-2 text-lg font-medium text-gray-800">*project name*</p>
+			<p class="text-xs text-gray-400">*project description*</p>
+			<p class="text-sm font-medium text-primary-500 text-end">View Details</p>
+		</div>
+	</div>
+</div>
