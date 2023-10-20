acb0a93f91c1f495e9623a94cd0c3292b8eee68d
Paul Minniti <pminniti@gmail.com>
2023-10-20 19:10:58 -0400

    updates

--- Client/src/lib/firebase.ts
+++ Client/src/lib/firebase.ts
@@ -6,15 +6,14 @@ import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { writable, type Readable, derived } from "svelte/store";
 import { docStore } from "./stores/docStore";
-import {
-	PUBLIC_FB_apiKey,
-	PUBLIC_FB_authDomain,
-	PUBLIC_FB_projectId,
-	PUBLIC_FB_storageBucket,
-	PUBLIC_FB_messagingSenderId,
-	PUBLIC_FB_appId,
-	PUBLIC_FB_measurementId
-} from "$env/static/public";
+
+const PUBLIC_FB_apiKey = "AIzaSyAHPng-HYy5uqslnxk1QnnPXwzdDcQ12ls";
+const PUBLIC_FB_authDomain = "ignitehub-d0696.firebaseapp.com";
+const PUBLIC_FB_projectId = "ignitehub-d0696";
+const PUBLIC_FB_storageBucket = "ignitehub-d0696.appspot.com";
+const PUBLIC_FB_messagingSenderId = "735417507395";
+const PUBLIC_FB_measurementId = "G-M22ZWDPS97";
+const PUBLIC_FB_appId = "1:735417507395:web:fad6444e2e76ed4b09bef6";
 
 const firebaseConfig = {
 	apiKey: PUBLIC_FB_apiKey,
