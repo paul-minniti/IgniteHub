8e52ae2b15ed371baca706c46aae72e790ba1366
Paul Minniti <pminniti@gmail.com>
2023-10-25 00:36:21 -0400

    fix routetopage host to by dynamic

--- Client/src/lib/navigation.ts
+++ Client/src/lib/navigation.ts
@@ -3,5 +3,5 @@
 import { goto } from "$app/navigation";
 
 export function routeToPage(route: string, replaceState: boolean = true) {
-	goto(`http://localhost:5173${route}`, { replaceState });
+	goto(`http://${window.location.host}${route}`, { replaceState });
 }
