ca41c17441249fff5d1189e3de78f8763d93f6bf
Paul Minniti <pminniti@gmail.com>
2025-02-11 01:51:24 -0500

    functions to module, add site.webmanifest, add new tables

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,1 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const next = require("next");
-
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev, conf: { distDir: ".next" } });
-const handle = app.getRequestHandler();
-
-const server = express();
-
-app.prepare().then(() => {
-	server.all("*", (req: Request, res: Response) => handle(req, res));
-});
-
-exports.nextApp = functions.https.onRequest(server);
+export { helloWorld } from "./hello.js";
